4052bc97054db416d5fb02445c49b10e50d720f6
Nathan <nathanrodriguez240@gmail.com>
2023-11-08 20:12:04 -0500

    update to server

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+var http = require('http'); 
+var https = require('https');
+
+http.createServer(app).listen(80); 
+https.createServer({}, app).listen(443);
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
